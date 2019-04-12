07a0058df7dc77c988439699034ca513a3af1dff
Ubuntu <ubuntu@ip-172-31-22-165.us-east-2.compute.internal>
2019-04-12 21:09:13 +0000

    updates

--- src/install_git.bash
+++ src/install_git.bash
@@ -5,6 +5,7 @@ cd /tmp
 /usr/bin/wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.21.0.tar.xz
 /usr/bin/unxz git-2.21.0.tar.xz
 /bin/tar xvf git-2.21.0.tar
+rm git-2.21.0.tar
 cd git-2.21.0
 export CC=/usr/bin/clang
 /usr/bin/make configure
@@ -12,4 +13,5 @@ export CC=/usr/bin/clang
 /usr/bin/make NO_TCLTK=YesPlease
 /usr/bin/apt remove -y git
 /usr/bin/make install
-
+cd /tmp
+rm -rf git-2.21.0
