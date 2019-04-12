f3474aa9b153772f87490f13e0f58d7b6d3c00c9
Ubuntu <ubuntu@ip-172-31-22-165.us-east-2.compute.internal>
2019-04-12 18:54:34 +0000

    updates

--- src/install_cmake.bash
+++ src/install_cmake.bash
@@ -4,6 +4,6 @@
 cd /opt
 /usr/bin/wget https://github.com/Kitware/CMake/releases/download/v3.14.2/cmake-3.14.2-Linux-x86_64.tar.gz
 /bin/tar xvf cmake-3.14.2-Linux-x86_64.tar.gz
-echo 'export PATH=$PATH:/opt/cmake-3.14.2-Linux-x86_64/bin' > /root/.bash_profile
+echo 'export PATH=$PATH:/opt/cmake-3.14.2-Linux-x86_64/bin' >> /root/.bashrc
 source /root/.bashrc
 /usr/bin/which cmake
